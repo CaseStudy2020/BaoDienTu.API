2609421be708498c320d836fc3e9edfcc5da90cf
HiepHoang2190 <hoangbachhiep2190@gmail.com>
2020-08-02 17:12:01 +0700

    Add-ASP.User and Register,Login Api

---
+++ BaoDienTu.Domain/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaoDienTu.Domain
+{
+   public class ApplicationUser:IdentityUser
+    {
+        public bool Gender { get; set; }
+        public DateTime DoB { get; set; }
+        public string Avatar { get; set; }
+        public string   Address { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
--- BaoDienTu/Startup.cs
+++ BaoDienTu/Startup.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BaoDienTu.API.DBContextt;
 using BaoDienTu.BAL;
 using BaoDienTu.BAL.Interface;
 using BaoDienTu.DAL;
 using BaoDienTu.DAL.Interface;
+using BaoDienTu.Domain;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +34,9 @@ namespace BaoDienTu
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Common.ConnectionString));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
             services.AddScoped<IPostRepository, PostRepository>();
             services.AddScoped<IPostService, PostService>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
