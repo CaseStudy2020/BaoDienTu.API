9ec10edd4927ffd9b21c695cb477ae4608fa4811
HiepHoang2190 <hoangbachhiep2190@gmail.com>
2020-08-03 13:51:12 +0700

    commit NumberOfComment

--- BaoDienTu.Domain/Response/Post/TopPostByDay.cs
+++ BaoDienTu.Domain/Response/Post/TopPostByDay.cs
@@ -10,17 +10,8 @@ namespace BaoDienTu.Domain.Response.Post
     {
         public int PostId { get; set; }
         public string Title { get; set; }
-        public string Content { get; set; }
-        public int Like { get; set; }
-        public string Link { get; set; }
-        public Status Status { get; set; }
-        public int CategoryId { get; set; }
-        public int PostHashTagId { get; set; }
-        public string AuthorId { get; set; }
-        public int SubCategoryId { get; set; }
-        public int View { get; set; }
-        [DataType(DataType.Date)]
         public string DateCreated { get; set; }
-        public decimal AverageRate { get; set; }
+        public int NumberOfComment { get; set; }
+        public string Thumbnail { get; set; }
     }
 }
