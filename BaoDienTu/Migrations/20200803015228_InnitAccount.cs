378f60fbf5fc09247c6926fbe64249e28f051461
vu <vshine94@gmail.com>
2020-08-04 10:53:07 +0700

    add migration

---
+++ BaoDienTu/Migrations/20200803015228_InnitAccount.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaoDienTu.API.Migrations
+{
+    public partial class InnitAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
