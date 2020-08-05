4e718bfeabebc707b5f002f89776e22683142f12
HiepHoang2190 <hoangbachhiep2190@gmail.com>
2020-08-05 12:01:27 +0700

    change common

--- BaoDienTu.Domain/Common.cs
+++ BaoDienTu.Domain/Common.cs
@@ -7,5 +7,7 @@ namespace BaoDienTu.Domain
     public class Common
     {
         public static string ConnectionString = @"Data Source=DESKTOP-FF1278R;Initial Catalog=Online-Newspaper-DB;Integrated Security=True";
+
+        ///
     }
 }
