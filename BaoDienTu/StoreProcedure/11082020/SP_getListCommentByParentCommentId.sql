d98f00b12968a1118a76d3b59e8e16b28eb195d1
Nguyen HuynhThao <tfosorcim1994@outlook.com>
2020-08-11 13:41:22 +0700

    NguyenHT-SP_getListCommentByParentComment

--- BaoDienTu/StoreProcedure/11082020/SP_getListCommentByParentCommentId.sql
+++ BaoDienTu/StoreProcedure/11082020/SP_getListCommentByParentCommentId.sql
@@ -13,23 +13,29 @@ GO
 CREATE PROCEDURE dbo.SP_getListCommentByParentCommentId
     @ParentCommentId int
 AS
-DECLARE @message NVARCHAR(100) = 'Not Found'
+DECLARE @message NVARCHAR(100)
 BEGIN
-    IF EXISTS ( select 1 from dbo.Comment c where c.CommentId = @ParentCommentId)
+    IF ( EXISTS ( select 1 from dbo.Comment c where c.CommentId = @ParentCommentId)
+        AND EXISTS(SELECT 1 FROM dbo.Comment c WHERE (c.ParentCommentId = @ParentCommentId and c.isDeleted = 0 and c.[status] = 0 )))
     BEGIN
         SET @message = 'Found'
-        SELECT *, @message as [Message] FROM dbo.Comment c WHERE (c.ParentCommentId = @ParentCommentId and c.isDeleted = 0 and c.[status] = 0 )
+        SELECT *, @message as [Message] 
+        FROM dbo.Comment c 
+        WHERE (c.ParentCommentId = @ParentCommentId and c.isDeleted = 0 and c.[status] = 0 )
+        ORDER BY c.DateCreated asc
     END
     ;
     ELSE
+    BEGIN
+        SET @message = 'Not found'
         SELECT @message as [Message]
+    END
+        
     ;
 END
-   
 GO
-
 -- example to execute the stored procedure we just created
-EXECUTE dbo.SP_getListCommentByParentCommentId 13
+EXECUTE dbo.SP_getListCommentByParentCommentId 1
 GO
 
 
